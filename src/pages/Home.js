78b740ca3204a2cd5099076a8156ed7a8b211178
ctavispost <ctavispost@gmail.com>
2020-11-26 18:24:10 -0800

    Set home page to retreive and show themes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ThemeModel from '../models/theme';
 
-const Home = () => {
-  return ( 
-    <main>
-      
-      <h1 className="hero">Theme Your Day</h1>
-      
+import { Link } from 'react-router-dom';
+import ThemeCard from '../components/ThemeCard';
 
-      <article className="grid"></article>
-    </main>
-  );
+class Home extends Component {
+  state = {
+    themes: []
+  };
+
+  componentDidMount() {
+    this.fetchData()
+  };
+
+  fetchData = () => {
+    ThemeModel.all().then(data => {
+      this.setState({ themes: data.theme })
+    });
+  }
+
+  render() {
+    let themeList = this.state.themes.map((theme, index) => {
+      return (
+        <Link to={`/theme/${ theme.id }`} key={index}>
+          <ThemeCard  {...theme} />
+        </Link>
+      )
+    });
+
+    return (
+      <div>
+        <h1 className="hero">Theme Your Day</h1>
+        { this.state.themes ? themeList : 'Loading...' }
+      </div>
+    );
+  }
 }
 
 export default Home;
