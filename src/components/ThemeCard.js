66006423f003360087aa071fc2770739174ef8e6
ctavispost <ctavispost@gmail.com>
2020-11-27 11:38:37 -0800

    Add return statement so ThemeCard's jsx gets returned

--- src/components/ThemeCard.js
+++ src/components/ThemeCard.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const ThemeCard = (props) => {
-    <article className="GameCard">
-        <h2>{ props.name }</h2>
-        <img src={ props.image } alt="{ props.title } art"/>
-    </article>
-}
+    return(
+        <article className="GameCard">
+            <h2>{ props.name }</h2>
+            <img src={ props.image } alt="{ props.title } art"/>
+        </article>
+    )
+};
 
 export default ThemeCard;
