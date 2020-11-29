b3dd915bca6818cbc66040a66f620a6252a0b2e6
ctavispost <ctavispost@gmail.com>
2020-11-28 17:06:10 -0800

    Get theme page to show theme and related activities

--- src/pages/Theme.js
+++ src/pages/Theme.js
@@ -1,11 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ThemeModel from '../models/theme';
+import ActModel from '../models/activity';
+import ThemeCard from '../components/ThemeCard';
+import ActCard from '../components/ActCard';
+import ThemeActModel from '../models/themeAct';
 
-const Theme = () => {
-  return (
-    <main>
-      <h1>Activities</h1>
-    </main>
-  )
-};
+class ShowTheme extends Component {
+  state = {
+    activities: [],
+    currentTheme: this.props.match.params.id
+  }
 
-export default Theme;
+  componentDidMount() {
+    this.getActivity()
+  }
+
+  getThemeAct = () => {
+    return(
+      ThemeActModel.allThemeActs()
+        .then(allTa => {
+          allTa.filter(themeAct => themeAct.themeId === this.state.currentTheme)
+      })
+    )
+  }
+
+  getActNum = (themeAct) => {
+    let nums = [];
+    themeAct.forEach(act => nums.push[act.actId]);
+    return nums;
+  }
+
+  getAct = (actIds) => {
+    return(
+      ActModel.allActs()
+        .then(allActivities => {
+          allActivities.filter(element => actIds.includes(element.id))
+          })
+            .catch(error => alert(error.message))
+    );
+  };
+  
+  getActivity = () => {
+    getThemeAct()
+      .then(themeActs => {
+        getAct(themeActs)
+          .then(foundActs => {
+            this.setState({ activities: foundActs.activities })
+          })
+            .catch(error => alert(error.message))
+      })
+      .catch(error => alert(error.message))
+  };
+
+  render() {
+    let actList = this.state.activities.map((activity, index) => {
+      return (<ActCard {...this.state.activities} />)
+    })
+
+    return (
+      <main>
+        <ThemeCard { ...this.state.currentTheme }/>
+        <article>
+          { this.state.activities ? actList : 'Loading...' }
+        </article>
+      </main>
+    )
+  }
+}
+
+export default ShowTheme;
