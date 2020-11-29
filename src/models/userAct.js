c35121dbc32da45c2f4b5751bfa009478020c51f
ctavispost <ctavispost@gmail.com>
2020-11-29 12:31:07 -0800

    Fix typo in filename

---
+++ src/models/userAct.js
@@ -0,0 +1,11 @@
+const url = `http://localhost:4000/api/v1`;
+
+class UserActModel {
+    static allUserActs = (userActId) => {
+        return fetch(`${url}/userAct/`)
+            .then(res => res.json()
+                .catch(error => alert(error.message))
+    }
+};
+
+export default UserActModel
