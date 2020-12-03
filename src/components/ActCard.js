8ae512e9daf4e47e96c782e33871240067b65093
ctavispost <ctavispost@gmail.com>
2020-12-03 14:22:00 -0800

    Style act cards to limit size of pics, and for spacing

--- src/components/ActCard.js
+++ src/components/ActCard.js
@@ -4,9 +4,9 @@ import React from 'react';
 
 const ActCard = (props) => {
     return(
-        <article className="ActCard">
+        <article className="actCard">
             <h2>{ props.title }</h2>
-            <img src={ props.image } alt="{ props.title } art"/>
+            <img src={ props.image } alt="{ props.title } art" className="activityPic"/>
             <p>{ props.description }</p>
             <button onClick={props.onDelete}>delete</button>
             <button onClick={props.onFavorite}>favorite</button>
